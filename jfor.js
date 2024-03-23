cc6df06ba97bd9bf4f647b8841dfa9c783cb9097
Lewis Musau <164549548+lesvil@users.noreply.github.com>
2024-03-23 17:57:01 +0300

    Add files via upload

---
+++ jfor.js
@@ -0,0 +1,5 @@
+document.getElementById('vacationForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    alert("Thank you for choosing Lewis Ventures, check your email to see packages available.");
+});
